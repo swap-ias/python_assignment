3ee8d96724cf45a0a1e36d4ee1a1247cc5e2a035
Sai Li <st.louski@gmail.com>
2023-04-14 13:53:58 +0900

    refactor: fetch raw data

--- get_raw_data.py
+++ get_raw_data.py
@@ -1,22 +1,33 @@
 import os
+from enum import Enum
 from typing import List
 
 import requests
 from model import Stocks
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from database import Dao
 from customized_logger import get_logger
 
 logger = get_logger("get_raw_data")
 
 
-def fetch_raw_stock_data(symbol: str, days_from_today: int = 14) -> List[Stocks]:
+class RawDataKeys(Enum):
+    TIME_SERIES = "Time Series (Daily)"
+    OPEN_PRICE = "1. open"
+    CLOSE_PRICE = "4. close"
+    VOLUME = "6. volume"
+    META_DATA = "Meta Data"
+    LAST_REFRESHED = "3. Last Refreshed"
+    DATE_FORMAT = "%Y-%m-%d"
+
+
+def fetch_raw_stock_data(symbol: str, days_from_last: int = 14) -> List[Stocks]:
     """
     Fetch raw stock data from alpha vantage site, convert and return the list of Stocks.
     The range is `days_from_today`, including today. Default range is 2 weeks.
 
     :param symbol: The symbol of stock.
-    :param days_from_today: specify the date range to be fetched. Default is 14 days from today inclusive.
+    :param days_from_last: specify the date range to be fetched. Default is 14 days from today inclusive.
     :return: a list of Stocks object.
     """
 
@@ -25,32 +36,31 @@ def fetch_raw_stock_data(symbol: str, days_from_today: int = 14) -> List[Stocks]
         logger.error("ALPHA_VANTAGE_API_KEY is not found in environment variable. Please setup the api key. Quit.")
         return
 
+    # fetch data from alpha vantage
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={alpha_vantage_api_key}"
     r = requests.get(url)
     data = r.json()
-    data = data['Time Series (Daily)']
     logger.info(f"Successfully fetched data of stock: {symbol}")
 
-    today = date.today()
+    # find the last refreshed date from meta data as the start_date.
+    start_date = data[RawDataKeys.META_DATA.value][RawDataKeys.LAST_REFRESHED.value]
+    start_date = datetime.strptime(start_date, RawDataKeys.DATE_FORMAT.value).date()
+    data = data[RawDataKeys.TIME_SERIES.value]
+
     result = []
-    for i in range(days_from_today):
-        target = (today - timedelta(days=i)).strftime("%Y-%m-%d")
+    # find most recent {days_from_last} data in the time series and add to result list.
+    for i in range(days_from_last):
+        target = (start_date - timedelta(days=i)).strftime(RawDataKeys.DATE_FORMAT.value)
         if target in data:
-            result.append(_convert_raw_to_stock(symbol, today - timedelta(days=i), data[target]))
+            stock = Stocks(symbol=symbol,
+                           date=start_date - timedelta(days=i),
+                           open_price=data[target][RawDataKeys.OPEN_PRICE.value],
+                           close_price=data[target][RawDataKeys.CLOSE_PRICE.value],
+                           volume=data[target][RawDataKeys.VOLUME.value]
+                           )
+            result.append(stock)
 
     return result
-
-
-def _convert_raw_to_stock(symbol, day, raw_data):
-    open_price_key = "1. open"
-    close_price_key = "4. close"
-    volume_key = "6. volume"
-    return Stocks(symbol=symbol,
-                  date=day,
-                  open_price=raw_data[open_price_key],
-                  close_price=raw_data[close_price_key],
-                  volume=raw_data[volume_key]
-                  )
 
 
 def main():
